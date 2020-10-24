58bbec6214a47397bf21d16cb84e32e79ea48b9a
za-jiangyang <yang.jiang@zatech.com>
2020-10-24 22:00:19 +0800

    启动

---
+++ spring-boot-tests/spring-boot-integration-tests/spring-boot-hibernate52-tests/src/main/java/org/springframework/boot/tests/hibernate52/controller/controller.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright By ZATI
+ * Copyright By 3a3c88295d37870dfd3b25056092d1a9209824b256c341f2cdc296437f671617
+ * All rights reserved.
+ *
+ * If you are not the intended user, you are hereby notified that any use, disclosure, copying, printing, forwarding or
+ * dissemination of this property is strictly prohibited. If you have got this file in error, delete it from your system.
+ */
+package org.springframework.boot.tests.hibernate52.controller;
+
+/**
+ * Created by za-jiangyang on 2020/10/24.
+ */
+
+public class controller {
+}
